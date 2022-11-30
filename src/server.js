ffcc9856d05f89798b32158c61649abcea1cfbac
Nicole Lee <nicolelee@Nicoles-MacBook-Pro.local>
2022-11-30 14:35:50 -0800

    built websocket server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import webSocket from "ws";
+import http from "http";
 import livereloadMiddleware from "connect-livereload";
 import livereload from "livereload";
+import {handle} from "express/lib/router";
 
 const app = express();
 
@@ -12,14 +15,20 @@ const liveServer = livereload.createServer({
 
 liveServer.watch(__dirname);
 
-app.use(livereloadMiddleware());
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
+app.use(livereloadMiddleware());
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 
 const handleListen = () => console.log(`Listening on local port 3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wsServer = new webSocket.Server( { server } );
+
+server.listen(3000, handleListen);
